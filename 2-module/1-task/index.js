010915d58818d362a1d330eb8be8c4fb595c2ee0
webmeteora <joeyjortravisbar@gmail.com>
2019-10-24 19:58:15 +0300

    task 1 module 2

--- 2-module/1-task/index.js
+++ 2-module/1-task/index.js
@@ -4,4 +4,16 @@
  * @returns {Object}
  */
 function clone(obj) {
+  if (obj == null || typeof obj !== 'object') {
+    return obj;
+  }
+  const copy = {};
+  for (const prop in obj) {
+    if (typeof obj[prop] === 'object') {
+      copy[prop] = clone(obj[prop]);
+    } else {
+      copy[prop] = obj[prop];
+    }
+  }
+  return copy;
 }
