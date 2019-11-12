5628215058b2d61203e75396a9d2bcfd39b25c62
evgen Mac <joeyjortravisbar@gmail.com>
2019-11-12 21:47:26 +0300

    task_1__module3

--- 3-module/1-task/index.js
+++ 3-module/1-task/index.js
@@ -5,4 +5,12 @@
  * @returns {string}
  */
 function showSalary(data, age) {
+  const people = data.filter(hum => hum.age <= age);
+  let result = '';
+
+  for (const key of people) {
+    result += (key.name + ',' + ' ' + key.balance + '\n');
+  }
+
+  return result.slice(0, -1);
 }
